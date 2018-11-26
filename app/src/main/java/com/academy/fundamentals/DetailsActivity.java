3e1dd5a769ffc1a91501db30c939cb9c087b5fb6
Roi <roigreenberg@gmail.com>
2018-11-27 00:15:38 +0200

    Initial commit

---
+++ app/src/main/java/com/academy/fundamentals/DetailsActivity.java
@@ -0,0 +1,24 @@
+package com.academy.fundamentals;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+
+    public void open_trailer(View view) {
+        Uri webpage = Uri.parse(getString(R.string.infinity_war_trailer));
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
